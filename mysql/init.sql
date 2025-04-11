457d89b5745fc6e95e80016dd3cb168d2a3fbd9f
fumitake-aies <fumitake@aies.jp>
2025-04-11 14:19:49 +0900

    Update init.sql

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS memos (
     id INT AUTO_INCREMENT PRIMARY KEY,  -- メモのID。自動インクリメントされ、一意の値が設定される
-    content TEXT,                       -- メモの内容。
+    content TEXT,                       -- メモの内容を格納
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,  -- 作成日時。デフォルトで現在のタイムスタンプが設定される
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP  -- 更新日時。デフォルトで現在のタイムスタンプが設定され、更新時に自動的に現在のタイムスタンプに更新される
 );
